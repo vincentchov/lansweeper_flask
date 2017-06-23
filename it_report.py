773ba79d045d354e13570b832f568bb70d11d6e5
Vincent Chov <vincentchov@gmail.com>
2017-06-23 14:23:45 -0400

    Use functions to automatically print the output of queries

--- it_report.py
+++ it_report.py
@@ -9,6 +9,13 @@ username = sql_user
 password = sql_passwd
 db = records.Database(db_url="mssql://{}:{}@{}".format(username, password, server))
 
-# Get the custom field info for your ticket number
-rows = db.query("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 98;")
-print(rows.dataset)
+def queryAndPrintDataSet(queryString):
+    rows = db.query(queryString)
+    print(rows.dataset)
+
+def queryAndPrintJSON(queryString):
+    rows = db.query(queryString)
+    print(json.dumps(json.loads(rows.export('json')), indent=4, sort_keys=True))
+
+# Example: Get the custom field info for your ticket number
+queryAndPrintDataSet("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 98;")
