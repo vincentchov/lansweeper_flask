c364e5752d9b3d50381b0151b23909f7318700ff
Vincent Chov <vincentchov@gmail.com>
2017-06-23 10:08:00 -0400

    Convert the project to use the Records library

--- it_report.py
+++ it_report.py
@@ -1,21 +1,14 @@
-import pyodbc
+import pyodbc, records, json
 from secrets import sql_user, sql_passwd
 
 # Log into the MS-SQL Server
-server = 'PSI-SQL' 
-database = 'lansweeperdb' 
+# Connect to the MS-SQL Server using a user-specified DSN.  I called mine "PSI-SQL-DSN"
+server = "PSI-SQL-DSN"
+database = "lansweeperdb"
 username = sql_user
 password = sql_passwd
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 11 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
+db = records.Database(db_url="mssql://{}:{}@{}".format(username, password, server))
 
 # Get the custom field info for your ticket number
-cursor.execute("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 98;") 
-row = cursor.fetchone()
-# Print out the column names
-field_names = [i[0] for i in cursor.description]
-print(field_names)
-# Print the rows returned
-while row: 
-    print(row)
-    row = cursor.fetchone()
+rows = db.query("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 98;")
+print(rows.dataset)
