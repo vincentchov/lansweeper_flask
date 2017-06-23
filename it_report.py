e7805e0230274137ba7db6eb1c5f3f167969e7d8
Vincent Chov <vincentchov@gmail.com>
2017-06-23 16:30:11 -0400

    Add sample SQL queries

--- it_report.py
+++ it_report.py
@@ -12,10 +12,23 @@ db = records.Database(db_url="mssql://{}:{}@{}".format(username, password, serve
 def queryAndPrintDataSet(queryString):
     rows = db.query(queryString)
     print(rows.dataset)
+    print("\n")
 
 def queryAndPrintJSON(queryString):
     rows = db.query(queryString)
     print(json.dumps(json.loads(rows.export('json')), indent=4, sort_keys=True))
+    print("\n")
 
-# Example: Get the custom field info for your ticket number
-queryAndPrintDataSet("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 98;")
+# # Example: Use a ticket number to match ticketcustomfield's field data with their field name
+# rows = db.query("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 100 ORDER BY htblticketcustomfield.fieldid;")
+# field_ids = []
+# # Get the field ids
+# for row in rows:
+#     field_ids.append(row.fieldid)
+#
+# for field_id in field_ids:
+#     queryAndPrintDataSet("SELECT * FROM htblcustomfields WHERE htblcustomfields.fieldid = {};".format(field_id))
+#     other_rows = db.query("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 100 AND htblticketcustomfield.fieldid = {};".format(field_id))
+#     print(other_rows.dataset, "\n\n\n============================================================================================================================= \n")
+
+# queryAndPrintDataSet("SELECT ticketid,assetid,tickettypeid FROM htblticket WHERE htblticket.ticketid = 100;")
