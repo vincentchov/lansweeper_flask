415950a130f18f8044ea644af2d547ef7611701b
Vincent Chov <vincentchov@gmail.com>
2017-06-26 11:58:06 -0400

    All info spread across rows

---
+++ All Info Spread Across Rows.sql
@@ -0,0 +1,28 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) htblticket.ticketid as TicketID
+	  ,htbltickettypes.typename as TicketType
+      ,htblticket.assetid as AssetID
+      ,htblticket.tickettypeid as TicketTypeID
+	  ,tblassets.AssetName as AssetName
+	  ,tsysAssetTypes.AssetTypename as AssetTypeName
+	  ,htblcustomfields.fieldid as FieldID
+	  ,htblcustomfields.name as FieldName
+	  ,htblticketcustomfield.data as FieldData
+  FROM [lansweeperdb].[dbo].[htblticket]
+  	/* FieldData */
+	INNER JOIN htblticketcustomfield 
+		ON htblticket.ticketid = htblticketcustomfield.ticketid
+	/* FieldName */
+	INNER JOIN htblcustomfields
+		ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
+	/* AssetName */
+	INNER JOIN tblassets
+		ON htblticket.assetid = tblassets.assetid
+	/* AssetTypeName */
+	INNER JOIN tsysAssetTypes
+		ON tblassets.AssetType = tsysAssetTypes.AssetType
+
+	/* TicketType */
+	INNER JOIN htbltickettypes
+		ON htblticket.tickettypeid = htbltickettypes.tickettypeid
+  WHERE htblticket.ticketid = 100;
