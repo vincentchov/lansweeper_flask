93981d16eee94ad93ba965d1296c5ad90638321e
Vincent Chov <vincentchov@gmail.com>
2017-06-27 17:18:21 -0400

    Add the SQL queries prior to joining

--- it_report.py
+++ it_report.py
@@ -1,4 +1,4 @@
-import pyodbc, records, json
+import pyodbc, records, json, pandas
 from secrets import sql_user, sql_passwd
 
 # Log into the MS-SQL Server
@@ -18,6 +18,15 @@ def queryAndPrintJSON(queryString):
     rows = db.query(queryString)
     print(json.dumps(json.loads(rows.export('json')), indent=4, sort_keys=True))
     print("\n")
+
+pre_pivoted_query = "SELECT TOP (1000) htblticketcustomfield.ticketid as [TicketID], htblticketcustomfield.fieldid as [FieldID], htblcustomfields.name as [FieldName], htblticketcustomfield.data as [FieldData] FROM [lansweeperdb].[dbo].[htblticketcustomfield] INNER JOIN htblcustomfields ON htblticketcustomfield.fieldid = htblcustomfields.fieldid WHERE [lansweeperdb].[dbo].[htblticketcustomfield].fieldid NOT IN (27,41,42,43,45,52,88) ORDER BY [TicketID],[FieldID];"
+
+df_to_pivot = pandas.read_json(db.query(pre_pivoted_query).export("json"))
+pivoted = pandas.pivot_table(df_to_pivot, index=['TicketID'], columns=['FieldName'], aggfunc=lambda x: ' '.join(x))
+
+to_join_query = "SELECT DISTINCT htblticket.ticketid as TicketID,htbltickettypes.typename as TicketType,tblassets.AssetName as AssetName,tsysAssetTypes.AssetTypename as AssetTypeName FROM [lansweeperdb].[dbo].[htblticket] INNER JOIN htblticketcustomfield ON htblticket.ticketid = htblticketcustomfield.ticketid INNER JOIN htblcustomfields ON htblticketcustomfield.fieldid = htblcustomfields.fieldid INNER JOIN tblassets ON htblticket.assetid = tblassets.assetid INNER JOIN tsysAssetTypes ON tblassets.AssetType = tsysAssetTypes.AssetType INNER JOIN htbltickettypes ON htblticket.tickettypeid = htbltickettypes.tickettypeid WHERE htbltickettypes.typename LIKE 'IT Support' AND htblticketcustomfield.FieldID IN (55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93, 94, 95, 96, 97, 100, 101, 103) ORDER BY htblticket.ticketid;"
+
+df_to_join = pandas.read_json(db.query(to_join_query).export("json"))
 
 # # Example: Use a ticket number to match ticketcustomfield's field data with their field name
 # rows = db.query("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 100 ORDER BY htblticketcustomfield.fieldid;")
