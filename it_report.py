c28419201e6b7763dd25d131bf874885d090ce86
Vincent Chov <vincentchov@gmail.com>
2017-06-28 12:37:42 -0400

    Add documentation comments and remove commented-out code

--- it_report.py
+++ it_report.py
@@ -19,25 +19,34 @@ def queryAndPrintJSON(queryString):
     print(json.dumps(json.loads(rows.export('json')), indent=4, sort_keys=True))
     print("\n")
 
-pre_pivoted_query = "SELECT TOP (1000) htblticketcustomfield.ticketid as [TicketID], htblticketcustomfield.fieldid as [FieldID], htblcustomfields.name as [FieldName], htblticketcustomfield.data as [FieldData] FROM [lansweeperdb].[dbo].[htblticketcustomfield] INNER JOIN htblcustomfields ON htblticketcustomfield.fieldid = htblcustomfields.fieldid WHERE [lansweeperdb].[dbo].[htblticketcustomfield].fieldid NOT IN (27,41,42,43,45,52,88) ORDER BY [TicketID],[FieldID];"
+# Write the query that gets all the FieldNames and FieldData for a given TicketID prior to pivoting
+pre_pivoted_query = "SELECT TOP (1000) htblticketcustomfield.ticketid as [TicketID], htblticketcustomfield.fieldid as [FieldID], htblcustomfields.name as [FieldName], htblticketcustomfield.data as [FieldData] FROM [lansweeperdb].[dbo].[htblticketcustomfield] INNER JOIN htblcustomfields ON htblticketcustomfield.fieldid = htblcustomfields.fieldid WHERE [lansweeperdb].[dbo].[htblticketcustomfield].fieldid IN (15, 55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93, 94, 95, 96, 97, 100, 101, 103) ORDER BY [TicketID],[FieldID];"
 
+# Pivot and then drop the FieldName level in the DataFrame
 df_to_pivot = pandas.read_json(db.query(pre_pivoted_query).export("json"))
 pivoted = pandas.pivot_table(df_to_pivot, index=['TicketID'], columns=['FieldName'], aggfunc=lambda x: ' '.join(x))
+pivoted.columns = pivoted.columns.droplevel()
 
-to_join_query = "SELECT DISTINCT htblticket.ticketid as TicketID,htbltickettypes.typename as TicketType,tblassets.AssetName as AssetName,tsysAssetTypes.AssetTypename as AssetTypeName FROM [lansweeperdb].[dbo].[htblticket] INNER JOIN htblticketcustomfield ON htblticket.ticketid = htblticketcustomfield.ticketid INNER JOIN htblcustomfields ON htblticketcustomfield.fieldid = htblcustomfields.fieldid INNER JOIN tblassets ON htblticket.assetid = tblassets.assetid INNER JOIN tsysAssetTypes ON tblassets.AssetType = tsysAssetTypes.AssetType INNER JOIN htbltickettypes ON htblticket.tickettypeid = htbltickettypes.tickettypeid WHERE htbltickettypes.typename LIKE 'IT Support' AND htblticketcustomfield.FieldID IN (55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93, 94, 95, 96, 97, 100, 101, 103) ORDER BY htblticket.ticketid;"
+# Write the query that gets the other information for a given TicketID
+to_join_query = "SELECT DISTINCT htblticket.ticketid as TicketID,htbltickettypes.typename as TicketType,tblassets.AssetName as AssetName,tsysAssetTypes.AssetTypename as AssetTypeName FROM [lansweeperdb].[dbo].[htblticket] INNER JOIN htblticketcustomfield ON htblticket.ticketid = htblticketcustomfield.ticketid INNER JOIN htblcustomfields ON htblticketcustomfield.fieldid = htblcustomfields.fieldid INNER JOIN tblassets ON htblticket.assetid = tblassets.assetid INNER JOIN tsysAssetTypes ON tblassets.AssetType = tsysAssetTypes.AssetType INNER JOIN htbltickettypes ON htblticket.tickettypeid = htbltickettypes.tickettypeid WHERE htbltickettypes.typename LIKE 'IT Support' AND htblticketcustomfield.FieldID IN (15, 55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93, 94, 95, 96, 97, 100, 101, 103) ORDER BY htblticket.ticketid;"
 
+# Perform the SQL query and store the results in a Pandas DataFrame
 df_to_join = pandas.read_json(db.query(to_join_query).export("json"))
 
-# # Example: Use a ticket number to match ticketcustomfield's field data with their field name
-# rows = db.query("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 100 ORDER BY htblticketcustomfield.fieldid;")
-# field_ids = []
-# # Get the field ids
-# for row in rows:
-#     field_ids.append(row.fieldid)
-#
-# for field_id in field_ids:
-#     queryAndPrintDataSet("SELECT * FROM htblcustomfields WHERE htblcustomfields.fieldid = {};".format(field_id))
-#     other_rows = db.query("SELECT * FROM htblticketcustomfield WHERE htblticketcustomfield.ticketid = 100 AND htblticketcustomfield.fieldid = {};".format(field_id))
-#     print(other_rows.dataset, "\n\n\n============================================================================================================================= \n")
+# Join the two DataFrames: pivoted and df_to_join
+joined = df_to_join.join(pivoted, on="TicketID")
 
-# queryAndPrintDataSet("SELECT ticketid,assetid,tickettypeid FROM htblticket WHERE htblticket.ticketid = 100;")
+# Reorder the columns so the final table starts with columns in first_cols_order
+first_cols_order = ['TicketID', 'TicketType',"Location of issue", "Asset Ownership ", "PSI Owned Asset Types", "Hardware Type"]
+joined_cols = joined.columns.tolist()
+
+for col in first_cols_order:
+    if joined_cols.index(col):
+        # Remove any instances of the above column names from the joined_cols list
+        del joined_cols[joined_cols.index(col)]
+
+# Concatenate the first_cols_order list with the remaining column names and set the column names
+joined_cols = first_cols_order + joined_cols
+joined = joined[joined_cols]
+
+joined.to_csv("joined.csv")
