17351b6144b163bc83130616e3431b8380b4867c
Vincent Chov <vchov@PSNET.LOCAL>
2017-06-30 14:01:13 -0400

    PEP8

--- it_report.py
+++ it_report.py
@@ -31,19 +31,19 @@ def queryAndPrintJSON(queryString):
 
 # Write the query that gets all the FieldNames and FieldData for a given
 # TicketID prior to pivoting
-pre_pivoted_query = (
-    "SELECT htblticketcustomfield.ticketid as [TicketID],"
-            "htblticketcustomfield.fieldid as [FieldID], "
-            "htblcustomfields.name as [FieldName], "
-            "htblticketcustomfield.data as [FieldData] "
-     "FROM [lansweeperdb].[dbo].[htblticketcustomfield] "
-     "INNER JOIN htblcustomfields "
-     "ON htblticketcustomfield.fieldid = htblcustomfields.fieldid "
-     "WHERE [lansweeperdb].[dbo].[htblticketcustomfield].fieldid IN "
-            "(15, 55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93, "
-            " 94, 95, 96, 97, 100, 101, 103) "
-     "ORDER BY [TicketID],[FieldID];"
-)
+pre_pivoted_query = """
+    SELECT htblticketcustomfield.ticketid as [TicketID],
+           htblticketcustomfield.fieldid as [FieldID],
+           htblcustomfields.name as [FieldName],
+           htblticketcustomfield.data as [FieldData]
+    FROM [lansweeperdb].[dbo].[htblticketcustomfield]
+    INNER JOIN htblcustomfields
+    ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
+    WHERE [lansweeperdb].[dbo].[htblticketcustomfield].fieldid IN
+        (15, 55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93,
+         94, 95, 96, 97, 100, 101, 103)
+ORDER BY [TicketID],[FieldID];
+"""
 
 # Pivot and then drop the FieldName level in the DataFrame
 df_to_pivot = pandas.read_json(db.query(pre_pivoted_query).export("json"))
@@ -54,28 +54,28 @@ pivoted = pandas.pivot_table(df_to_pivot,
 pivoted.columns = pivoted.columns.droplevel()
 
 # Write the query that gets the other information for a given TicketID
-to_join_query = (
-    "SELECT DISTINCT htblticket.ticketid as TicketID, "
-                    "htbltickettypes.typename as TicketType, "
-                    "tblassets.AssetName as AssetName,"
-                    "tsysAssetTypes.AssetTypename as AssetTypeName "
-     "FROM [lansweeperdb].[dbo].[htblticket] "
-     "INNER JOIN htblticketcustomfield "
-        "ON htblticket.ticketid = htblticketcustomfield.ticketid "
-     "INNER JOIN htblcustomfields "
-        "ON htblticketcustomfield.fieldid = htblcustomfields.fieldid "
-     "INNER JOIN tblassets "
-        "ON htblticket.assetid = tblassets.assetid "
-     "INNER JOIN tsysAssetTypes "
-        "ON tblassets.AssetType = tsysAssetTypes.AssetType "
-     "INNER JOIN htbltickettypes "
-        "ON htblticket.tickettypeid = htbltickettypes.tickettypeid "
-     "WHERE htbltickettypes.typename "
-        "LIKE 'IT Support' AND htblticketcustomfield.FieldID IN "
-        "(15, 55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93, "
-        " 94, 95, 96, 97, 100, 101, 103) "
-     "ORDER BY htblticket.ticketid;"
-)
+to_join_query = """
+    SELECT DISTINCT htblticket.ticketid as TicketID,
+                    htbltickettypes.typename as TicketType,
+                    tblassets.AssetName as AssetName,
+                    tsysAssetTypes.AssetTypename as AssetTypeName
+     FROM [lansweeperdb].[dbo].[htblticket]
+     INNER JOIN htblticketcustomfield
+        ON htblticket.ticketid = htblticketcustomfield.ticketid
+     INNER JOIN htblcustomfields
+        ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
+     INNER JOIN tblassets
+        ON htblticket.assetid = tblassets.assetid
+     INNER JOIN tsysAssetTypes
+        ON tblassets.AssetType = tsysAssetTypes.AssetType
+     INNER JOIN htbltickettypes
+        ON htblticket.tickettypeid = htbltickettypes.tickettypeid
+     WHERE htbltickettypes.typename
+        LIKE 'IT Support' AND htblticketcustomfield.FieldID IN
+        (15, 55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93,
+         94, 95, 96, 97, 100, 101, 103)
+     ORDER BY htblticket.ticketid;
+"""
 
 # Perform the SQL query and store the results in a Pandas DataFrame
 df_to_join = pandas.read_json(db.query(to_join_query).export("json"))
