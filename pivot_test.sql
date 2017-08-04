2629e396484013371a05d385d0b599a47b9b142b
Vincent Chov <vincentchov@gmail.com>
2017-08-04 17:27:21 -0400

    Formatting

--- pivot_test.sql
+++ pivot_test.sql
@@ -1,58 +1,84 @@
-/* Reset the temporary pivot table */
-
-if not exists (SELECT * FROM sys.tables where name='PrePivotTempTable')
-	CREATE table PrePivotTempTable
-		([TicketID]  int,
-		 [FieldID]   int,
-		 [FieldName] nvarchar(max),
-		 [FieldData] nvarchar(max)
-	)
-go
-
-DELETE FROM PrePivotTempTable
-
-/* Populate the PrePivotTempTable with the FieldName and FieldData to be pivotted */
-
-INSERT INTO 
-    PrePivotTempTable ([TicketID],[FieldID],[FieldName],[FieldData])
-SELECT TOP (1000) htblticketcustomfield.ticketid as [TicketID]
-	  , htblticketcustomfield.fieldid as [FieldID]
-	  , htblcustomfields.name as [FieldName]
-	  , htblticketcustomfield.data as [FieldData]
-  FROM [lansweeperdb].[dbo].[htblticketcustomfield]
-	INNER JOIN htblcustomfields
-		ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
-  WHERE [lansweeperdb].[dbo].[htblticketcustomfield].fieldid NOT IN (27,41,42,43,45,52,88)
-  ORDER BY [TicketID],[FieldID];
-
-/* Build the dynamic query that performs the pivotting */
-
 DECLARE @cols AS NVARCHAR(MAX),
-    @query  AS NVARCHAR(MAX)
+        @query  AS NVARCHAR(MAX)
 
-select @cols = STUFF((SELECT DISTINCT ',' + QUOTENAME(FieldName)
-                    from (select DISTINCT  
-				 TicketID
-				, FieldData
-				, FieldName
-                from PrePivotTempTable
-				) x
-				group by  FieldName, TicketID
-            FOR XML PATH(''), TYPE
-            ).value('.', 'NVARCHAR(MAX)')
-		,1,1,'')
+SELECT @cols =  STUFF((
+    SELECT DISTINCT ',' + QUOTENAME(FieldName)
+    FROM (
+        SELECT DISTINCT TicketID, FieldData, FieldName
+        FROM (
+            SELECT TOP 1000 htblticketcustomfield.ticketid as [TicketID],
+                htblticketcustomfield.fieldid as [FieldID],
+                htblcustomfields.name as [FieldName],
+                htblticketcustomfield.data as [FieldData]
+            FROM [lansweeperdb].[dbo].[htblticketcustomfield]
+            INNER JOIN htblcustomfields
+                ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
+            WHERE htblticketcustomfield.fieldid
+                NOT IN (27,41,42,43,45,52,88)
+            ORDER BY [TicketID],[FieldID]
+        ) y
+     ) x
+     GROUP BY FieldName, TicketID
+     FOR XML
+        PATH(''),
+        TYPE
+     ).value('.', 'NVARCHAR(MAX)'),
+     1,
+     1,
+     ''
+)
 
-set @query = 'SELECT DISTINCT TicketID AS TicketID, ' + @cols + ' from 
-             (
-                select TicketID
-				, FieldData
-				, FieldName
-                from PrePivotTempTable
-            ) x
-            pivot 
-            (
-                max(FieldData)
-                for FieldName in (' + @cols + ')
-            ) p '
-
+SET @query = '
+    WITH to_join (TicketID, FieldID, FieldName, FieldData)
+    AS (
+        SELECT DISTINCT TOP 10000
+            htblticket.ticketid as TicketID,
+            htbltickettypes.typename as TicketType,
+            tblassets.AssetName as AssetName,
+            tsysAssetTypes.AssetTypename as AssetTypeName
+         FROM [lansweeperdb].[dbo].[htblticket]
+         INNER JOIN htblticketcustomfield
+            ON htblticket.ticketid = htblticketcustomfield.ticketid
+         INNER JOIN htblcustomfields
+            ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
+         INNER JOIN tblassets
+            ON htblticket.assetid = tblassets.assetid
+         INNER JOIN tsysAssetTypes
+            ON tblassets.AssetType = tsysAssetTypes.AssetType
+         INNER JOIN htbltickettypes
+            ON htblticket.tickettypeid = htbltickettypes.tickettypeid
+         WHERE htbltickettypes.typename
+            LIKE ''IT Support'' AND htblticketcustomfield.FieldID IN
+            (15, 55, 60, 72, 81, 83, 84, 85, 89, 90, 91, 92, 93,
+             94, 95, 96, 97, 100, 101, 103)
+         ORDER BY htblticket.ticketid
+    ),
+    pre_pivoted (TicketID, FieldID, FieldName, FieldData)
+    AS (
+        SELECT TOP 10000
+            htblticketcustomfield.ticketid as [TicketID],
+            htblticketcustomfield.fieldid as [FieldID],
+            htblcustomfields.name as [FieldName],
+            htblticketcustomfield.data as [FieldData]
+        FROM [lansweeperdb].[dbo].[htblticketcustomfield]
+        INNER JOIN htblcustomfields
+            ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
+        WHERE htblticketcustomfield.fieldid NOT IN (27,41,42,43,45,52,88)
+        ORDER BY [TicketID],[FieldID]
+    )
+    SELECT * FROM to_join
+    LEFT JOIN (
+    SELECT DISTINCT TicketID AS TicketID, ' +
+        @cols + '
+        FROM (
+            SELECT TicketID, FieldData, FieldName
+            FROM pre_pivoted
+        ) x
+        PIVOT (
+            MAX(FieldData)
+            FOR FieldName IN (' + @cols + ')
+        ) p
+    ) y
+    ON y.TicketID = to_join.TicketID
+    '
 EXEC(@query);
