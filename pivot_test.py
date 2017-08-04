4bca7a1a43cd2b3a201ca0902658d5efdbb68dff
Vincent Chov <vincentchov@gmail.com>
2017-08-04 09:57:54 -0400

    Demonstrate use of CTEs

--- pivot_test.py
+++ pivot_test.py
@@ -10,32 +10,23 @@ db = records.Database(db_url="mssql://" + server)
 # Write the query that gets all the FieldNames and FieldData for a given
 # TicketID prior to pivoting
 pivoted_query = """
-    DECLARE @cols AS NVARCHAR(MAX),
-            @query  AS NVARCHAR(MAX)
+    DECLARE @query  AS NVARCHAR(MAX)
 
-    SELECT @cols = STUFF((SELECT DISTINCT ',' + QUOTENAME(FieldName)
-                             FROM (SELECT DISTINCT TicketID
-                                  , FieldData
-                                  , FieldName
-                                  FROM PrePivotTempTable
-                                  ) x
-                             GROUP BY FieldName, TicketID
-                             FOR XML PATH(''),
-                                        TYPE)
-                            .value('.', 'NVARCHAR(MAX)'
-                         ) ,1,1,'')
-
-    SET @query = 'SELECT DISTINCT TicketID AS TicketID, ' + @cols +
-                 ' FROM (
-                       SELECT TicketID, FieldData, FieldName
-                       FROM PrePivotTempTable
-                   ) x
-                   PIVOT
-                   (
-                       MAX(FieldData)
-                       FOR FieldName IN (' + @cols + ')
-                   ) p
-                 '
+    SET @query = '
+        WITH pre_pivoted (TicketID, FieldID, FieldName, FieldData)
+        AS (
+            SELECT TOP 1000 htblticketcustomfield.ticketid as [TicketID],
+                 htblticketcustomfield.fieldid as [FieldID],
+                 htblcustomfields.name as [FieldName],
+                 htblticketcustomfield.data as [FieldData]
+            FROM [lansweeperdb].[dbo].[htblticketcustomfield]
+            INNER JOIN htblcustomfields
+              ON htblticketcustomfield.fieldid = htblcustomfields.fieldid
+            WHERE htblticketcustomfield.fieldid NOT IN (27,41,42,43,45,52,88)
+            ORDER BY [TicketID],[FieldID]
+        )
+        SELECT * FROM pre_pivoted
+        '
     EXEC(@query);
 """
 
