c43a327601629e001a756607dbbc9372255f3ca0
Vincent Chov <vincentchov@gmail.com>
2017-08-28 09:22:34 -0400

    Add all the report types' information to constants.py

--- constants.py
+++ constants.py
@@ -25,3 +25,68 @@ IT_SUPPORT = {
                     tsysAssetTypes.AssetTypename,\n
                 """
 }
+
+CUSTOMER_VENDOR_RELATED = {
+    "fieldid": "(124,125,126,127,128)",
+    "typename": "Customer / Vendor Related"
+}
+
+EH_AND_S = {
+    "fieldid": "(15,41,123)",
+    "typename": "EH & S - Environmental Health & Safety"
+}
+
+ENGINEERING = {
+    "fieldid": "(15,108,110)",
+    "typename": "Engineering "
+}
+
+EXPORT_COMPLIANCE = {
+    "fieldid": "(15,106)",
+    "typename": "Export Compliance"
+}
+
+FACILITY_MAINTENANCE = {
+    "fieldid": "(15,43,41,42,142,45)",
+    "typename": "Facility / Maintenance"
+}
+
+HUMAN_RESOURCES = {
+    "fieldid": "(15,130,133,131)",
+    "typename": "Human Resources"
+}
+
+PROJ_MGMT_MGMT_ISSUES = {
+    "fieldid": "(15,146,147,148,150)",
+    "typename": "Project Management / Management Issues"
+}
+
+PURCHASING_AND_ACCOUNTING = {
+    "fieldid": "(15,109,110)",
+    "typename": "Purchasing & Accounting"
+}
+
+QUALITY = {
+    "fieldid": "(151,153,152)",
+    "typename": "Quality"
+}
+
+SECURITY = {
+    "fieldid": "(15,50,51,53,52,45,135,137,136,138)",
+    "typename": "Security "
+}
+
+SHOP_OPERATIONS = {
+    "fieldid": "(41,112,116,111,114,115,113,120,117,118,122,121)",
+    "typename": "Shop Operations"
+}
+
+TIMESHEET_SYSTEM = {
+    "fieldid": "(129,110)",
+    "typename": "Timesheet System"
+}
+
+TRAINING = {
+    "fieldid": "(15,107)",
+    "typename": "Training"
+}
