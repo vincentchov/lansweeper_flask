b053e1c41a08e3e7af1c7f43b5243ddd6ad632f4
Vincent Chov <vincentchov@gmail.com>
2017-08-28 10:22:35 -0400

    Delete Old Reports and SQL Backups

--- SQL_Backup/remake_json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-filename = 'tsysAssetTypes.json'
-
-with open(filename) as json_data:
-    d = json.load(json_data)
-
-with open(filename, 'w') as json_data:
-    json_data.write(json.dumps(d, indent=4, sort_keys=True))
