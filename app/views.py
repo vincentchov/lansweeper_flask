24e8b52bfc1001938fe2e7c22efa59e30d7788c9
Vincent Chov <vincentchov@gmail.com>
2017-09-07 15:53:14 -0400

    Bootstrap + JQuery.  Return success or failure with a template

--- app/views.py
+++ app/views.py
@@ -1,13 +1,14 @@
+import json
+from config import *
 from app import app, db
+from sqlalchemy.exc import ResourceClosedError
 from .lansweeper_all import execute_report_given_option
 from flask import redirect, flash, request, url_for, jsonify, render_template
-from config import *
-import json
 
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    return render_template('index.html')
 
 
 @app.route('/reports/<option_int>')
@@ -15,6 +16,10 @@ def reports(option_int):
     try:
         option_int = int(option_int)
         execute_report_given_option(option_int)
-        return "Success!"
+        message = "Success!"
     except ValueError as e:
-        return str(e)
+        message = str(e)
+    except ResourceClosedError as e:
+        message = "No tickets found!"
+
+    return render_template('reports.html', message=message)
